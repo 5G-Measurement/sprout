7a5c0029dcbe43a2d00aa129e8092c4cb6fdcdc2
ahmad <ahmadhassansandhu@gmail.com>
2020-12-05 14:07:45 -0600

    server -> client data transfer + logging + time duration

--- src/examples/sproutbt2.cc
+++ src/examples/sproutbt2.cc
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <list>
 #include <iostream>
+#include <fstream>
+#include <sys/time.h>
 
 #include "sproutconn.h"
 #include "select.h"
@@ -14,12 +16,16 @@ int main( int argc, char *argv[] )
 {
   char *ip;
   int port;
+  std::ofstream s_logger;
+  std::ofstream r_logger;
+  char command[512];
+  double timeToRun;
 
   Network::SproutConnection *net;
 
   bool server = true;
 
-  if ( argc > 2 ) {
+  if ( argc == 5 ) {
     /* client */
 
     server = false;
@@ -28,15 +34,28 @@ int main( int argc, char *argv[] )
     port = atoi( argv[ 2 ] );
 
     net = new Network::SproutConnection( "4h/Td1v//4jkYhqhLGgegw", ip, port );
-  } else if ( argc == 2 ) {
+    sprintf (command, "%s_client_send.csv", argv[3]);
+    s_logger.open(command);
+    sprintf (command, "%s_client_recv.csv", argv[3]);
+    r_logger.open(command);
+    timeToRun = stod(argv[4]);
+  } 
+  else if ( argc == 4 ) {
     net = new Network::SproutConnection( NULL, argv[ 1 ] );
 
     printf( "Listening on port: %d\n", net->port() );
-  } else {
-    net = new Network::SproutConnection( NULL, NULL );
-
-    cout << "Listening on port: " << net->port() << endl;
+    sprintf (command, "%s_server_send.csv", argv[2]);
+    s_logger.open(command);
+    sprintf (command, "%s_server_recv.csv", argv[2]);
+    r_logger.open(command);
+    timeToRun = stod(argv[3]);
+  } 
+  else {
+    printf( "usage: sproutbt2 [IP] port filename duration" );
+    exit(1);
   }
+
+  
 
   Select &sel = Select::get_instance();
   sel.add_fd( net->fd() );
@@ -45,51 +64,63 @@ int main( int argc, char *argv[] )
 
   /* wait to get attached */
   if ( server ) {
+
     while ( 1 ) {
       int active_fds = sel.select( -1 );
       if ( active_fds < 0 ) {
-	perror( "select" );
-	exit( 1 );
+        perror( "select" );
+        exit( 1 );
       }
 
       if ( sel.read( net->fd() ) ) {
-	net->recv();
+        net->recv();
       }
 
       if ( net->get_has_remote_addr() ) {
-	break;
+        break;
       }
     }
+
   }
+
+  struct timeval currentTime;
+  struct timeval startTime;
+  double relativeTime = 0;
+  gettimeofday(&startTime,NULL);
 
   uint64_t time_of_next_transmission = timestamp() + fallback_interval;
 
   fprintf( stderr, "Looping...\n" );  
 
   /* loop */
-  while ( 1 ) {
+  while ( relativeTime <= timeToRun ) 
+  {
+    gettimeofday(&currentTime, NULL);
+    relativeTime = (currentTime.tv_sec- startTime.tv_sec)+(currentTime.tv_usec-startTime.tv_usec)/1000000.0;
     int bytes_to_send = net->window_size();
 
-    if ( server ) {
+    if ( !server ) {
       bytes_to_send = 0;
     }
 
     /* actually send, maybe */
     if ( ( bytes_to_send > 0 ) || ( time_of_next_transmission <= timestamp() ) ) {
       do {
-	int this_packet_size = std::min( 1440, bytes_to_send );
-	bytes_to_send -= this_packet_size;
-	assert( bytes_to_send >= 0 );
+        int this_packet_size = std::min( 1440, bytes_to_send );
+        bytes_to_send -= this_packet_size;
+        assert( bytes_to_send >= 0 );
 
-	string garbage( this_packet_size, 'x' );
+        string garbage( this_packet_size, 'x' );
 
-	int time_to_next = 0;
-	if ( bytes_to_send == 0 ) {
-	  time_to_next = fallback_interval;
-	}
+        int time_to_next = 0;
+        if ( bytes_to_send == 0 ) {
+          time_to_next = fallback_interval;
+        }
 
-	net->send( garbage, time_to_next );
-      } while ( bytes_to_send > 0 );
+	      net->send( garbage, time_to_next );
+        s_logger << garbage.size() << "," << time_to_next << "\n";
+      } 
+      while ( bytes_to_send > 0 );
 
       time_of_next_transmission = std::max( timestamp() + fallback_interval,
 					    time_of_next_transmission );
@@ -99,7 +130,8 @@ int main( int argc, char *argv[] )
     int wait_time = time_of_next_transmission - timestamp();
     if ( wait_time < 0 ) {
       wait_time = 0;
-    } else if ( wait_time > 10 ) {
+    } 
+    else if ( wait_time > 10 ) {
       wait_time = 10;
     }
 
@@ -112,6 +144,9 @@ int main( int argc, char *argv[] )
     /* receive */
     if ( sel.read( net->fd() ) ) {
       string packet( net->recv() );
+      r_logger << packet.size() << "\n";
     }
   }
+  s_logger.close();
+  r_logger.close();
 }
